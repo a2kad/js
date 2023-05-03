56ddd577e36fb85c0c82544424e48531a491e7a4
a2kad <reshetnikovruslan705@gmail.com>
2023-05-03 13:41:57 +0200

    Finished exo5

--- base/5/assets/js/script.js
+++ base/5/assets/js/script.js
@@ -1,4 +1,26 @@
 // Je declare ma fonction
-function makeAlert(){
+function makeAlert() {
     window.alert('BOOM !')
 }
+const monBouton = document.getElementById('myButton')
+
+const myButton = document.querySelector('#myButton')
+//nomBouton.onclick = makeAlert
+
+monBouton.addEventListener("click", function () {
+    makeAlert()
+})
+function writeMessage(name) {
+    let myDiv = document.getElementById('message')
+    myDiv.innerText = `Bonjour, ${name}, comment allez-vous?`
+}
+let mxButton = document.getElementById('mxButton')
+mxButton.addEventListener('click', function () {
+    let inputFirstName = document.getElementById('inputFirstName')
+    let name = inputFirstName.value
+    if (name == '') {
+        window.alert('Enter the name')
+    } else {
+        writeMessage(name)
+    }
+})
