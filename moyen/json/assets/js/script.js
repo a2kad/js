6f4ca1c10e29991e7965088689d055ca55712acc
a2kad <reshetnikovruslan705@gmail.com>
2023-05-24 13:27:52 +0200

    fifnished json project

--- moyen/json/assets/js/script.js
+++ moyen/json/assets/js/script.js
@@ -41,16 +41,38 @@ const data = {
 }
 
 const row1 = document.getElementById('row1')
+const listOfHeros = document.getElementById('listOfHeros')
 
-row1.innerHTML = `<h1>${data.squadName}</h1>`
+row1.innerHTML = `
+<h1>${data.squadName}</h1>
+<p>Hometown: ${data.homeTown} // Formed: ${data.formed}</p>
+`
 
 data.members.forEach(element => {
     console.log(element);
+    let colOfHero = document.createElement('div')
+    listOfHeros.appendChild(colOfHero)
+    
+    
+    colOfHero.classList.add('col')
+    colOfHero.innerHTML = `
+    <h2>${element.name}</h2>
+    <p>Secret identity: ${element.secretIdentity}</p>
+    <p>Age: ${element.age}</p>
+    
+    <p>Superpowers:</p>`
+        element.powers.forEach(element =>{
+            let listPower = document.createElement('ul')
+            colOfHero.appendChild(listPower)
+            listPower.classList.add('list-group')
+            listPower.innerHTML = `<li class="list-group-item">${element}</li>`
+        })
+
 })
 
-for (let i = 0; i < 3; i++) {
-    let colName = 'col' + (i + 1)
-    console.log(colName)
-    document.getElementById(`${colName}`).innerHTML = `<p>${data.members[i].name}</p>`
+// for (let i = 0; i < 3; i++) {
+//     let colName = 'col' + (i + 1)
+//     console.log(colName)
+//     document.getElementById(`${colName}`).innerHTML = `<p>${data.members[i].name}</p>`
     
-}
+// }
