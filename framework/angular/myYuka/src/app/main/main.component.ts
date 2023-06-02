66f0c8095af945ae1736d1d1f336463267c66ac4
a2kad <reshetnikovruslan705@gmail.com>
2023-06-02 20:04:36 +0200

    nova nutri

--- framework/angular/myYuka/src/app/main/main.component.ts
+++ framework/angular/myYuka/src/app/main/main.component.ts
@@ -18,8 +18,13 @@ export class MainComponent {
   public prodNutriScore: string;
   public prodPalme: string;
   public stores: string;
+  public prodNovaIndex : string;
 
   productSearch = new FormControl('');
+
+  submitted = false;
+
+  onSubmit() { this.submitted = true; }
 
   public prodCode: string;
 
@@ -33,6 +38,7 @@ export class MainComponent {
     this.prodNutriScore = '';
     this.prodPalme = '';
     this.stores = '';
+    this.prodNovaIndex = '';
   }
 
   public showProductInfo() {
@@ -42,19 +48,36 @@ export class MainComponent {
           this.prodIngredinetns = data['product']['ingredients_text_fr'],
           this.prodPhoto = data['product']['image_url'],
           this.prodAllergen = data['product']['allergens_from_ingredients'],
-          //this.prodNova = data['product'].nova_groupnova_groups_tags[Object.keys(data['product'].nova_groupnova_groups_tags)[0]]
-          this.prodNova = data['product']['nova_groups']
+
+        //  NOVA
+
+        this.prodNovaIndex = data['product']['nova_groups']
+        if (this.prodNovaIndex == '1') {
+          this.prodNova = "../../assets/images/320px-NOVA_group_1.svg.png"
+        } else if (this.prodNovaIndex == '2') {
+          this.prodNova = "../../assets/images/320px-NOVA_group_2.svg.png"
+        } else if (this.prodNovaIndex == '3') {
+          this.prodNova = "../../assets/images/320px-NOVA_group_3.svg.png"
+        } else if (this.prodNovaIndex == '4') {
+          this.prodNova = "../../assets/images/320px-NOVA_group_4.svg.png"
+        } else{
+          this.prodNova = "../../assets/images/320px-NOVA_group_inconnu.svg.png"
+        }
+
+        // Nutri Score
         this.prodNutriScoreIndex = data['product']['nutrition_grade_fr']
         if (this.prodNutriScoreIndex == 'a') {
-          this.prodNutriScore = `<img src="../../assets/images/240px-Nutri-score-A.svg.png" class="imgScan img-fluid" alt="Scan barcode">`
-        }else if (this.prodNutriScoreIndex == 'b') {
-          this.prodNutriScore = `<img src="../../assets/images/240px-Nutri-score-B.svg.png" class="imgScan img-fluid" alt="Scan barcode">`
-        }else if (this.prodNutriScoreIndex == 'c') {
-          this.prodNutriScore = `<img src="../../assets/images/240px-Nutri-score-C.svg.png" class="imgScan img-fluid" alt="Scan barcode">`
-        }else if (this.prodNutriScoreIndex == 'd') {
-          this.prodNutriScore = `<img src="../../assets/images/240px-Nutri-score-D.svg.png" class="imgScan img-fluid" alt="Scan barcode">`
-        }else if (this.prodNutriScoreIndex == 'e') {
-          this.prodNutriScore = `<img src="../../assets/images/240px-Nutri-score-E.svg.png" class="imgScan img-fluid" alt="Scan barcode">`
+          this.prodNutriScore = "../../assets/images/240px-Nutri-score-A.svg.png"
+        } else if (this.prodNutriScoreIndex == 'b') {
+          this.prodNutriScore = "../../assets/images/240px-Nutri-score-B.svg.png"
+        } else if (this.prodNutriScoreIndex == 'c') {
+          this.prodNutriScore = "../../assets/images/240px-Nutri-score-C.svg.png"
+        } else if (this.prodNutriScoreIndex == 'd') {
+          this.prodNutriScore = "../../assets/images/240px-Nutri-score-D.svg.png"
+        } else if (this.prodNutriScoreIndex == 'e') {
+          this.prodNutriScore = "../../assets/images/240px-Nutri-score-E.svg.png"
+        }else{
+          this.prodNutriScore = "../../assets/images/240px-Nutri-score-Inconnu.svg .png"
         }
 
         this.prodPalme = data['product']['ingredients_from_palm_oil_n']
